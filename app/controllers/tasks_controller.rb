8c5f82fb50236dfe8b9b4691b6bd01ba8fe91208
taishi.miyai <taishimiyai@gmai.com>
2018-09-07 09:55:22 +0000

    プッシュ

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,53 @@
+class TasksController < ApplicationController
+  def index
+     @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+     @task = Task.new
+  end
+
+  def create
+     @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = 'task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+      @tasks = Tsak.all.find(params[:id])
+  end
+
+  def update
+    @tasks = Tsak.all.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.all.find(params[:id])
+     @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def task_params
+  end
+end
