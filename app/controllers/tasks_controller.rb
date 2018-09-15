4b9ac5ab439c8776af01ea1988766c1e1968f6e2
taishi.miyai <taishimiyai@gmai.com>
2018-09-15 13:05:59 +0000

    bootstarp課題

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-     @tasks = Task.all
+     @tasks = Task.all.page(params[:page])
   end
 
   def show
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
   end
 
   def update
-    
+    binding.pry
  @task = Task.new(task_params)
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
